a4ad788aa2ded21598d04a09ea9a879fc8c380cc
leaderproxima <bill.chandler94@yahoo.co.uk>
2015-12-08 09:32:48 +0000

    Created distribution version of Stronghold V1.0. This version can be used and is fully working but there is much more to add. Includes a test page that shows off most of the features

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+var config = {
+  /* The interval between changing images on banner (1000 = 1 second) */
+  "bannerChangeInterval": 5000,
+  /* 100(quick) - 900(slow)*/
+  "bannerAnimationSpeed": 500
+};
